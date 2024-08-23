d50fc6df24b1c889bf0deb9520723c8609f7dc93
Gabriel Ebner <gebner@gebner.org>
2024-08-23 10:46:36 -0700

    Use single-line comments.

--- ftplugin/fstar/fstar.lua
+++ ftplugin/fstar/fstar.lua
@@ -5,7 +5,7 @@ vim.b.did_ftplugin = 1
 
 vim.opt.wildignore:append [[*.checked]]
 vim.bo.comments = [[s0:(*,mb:\ ,ex:*),://]]
-vim.bo.commentstring = [[(* %s *)]]
+vim.bo.commentstring = [[// %s]]
 
 vim.bo.expandtab = true
 vim.bo.shiftwidth = 2
